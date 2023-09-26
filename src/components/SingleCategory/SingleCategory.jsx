f68e6247efdc22a50cbaf75c91dcedcb45bdd3b2
rezwan2230 <rezwan2230@gmail.com>
2023-09-27 02:09:22 +0600

    added active route css and change href to link for all donations

--- src/components/SingleCategory/SingleCategory.jsx
+++ src/components/SingleCategory/SingleCategory.jsx
@@ -1,55 +1,22 @@
-// import { Link } from "react-router-dom";
-
-// const SingleCategory = ({singleData}) => {
-//     const {id, img, category, title, cardBgColor, categoryBgColor, TEXTColor } = singleData;
-//     // console.log(singleData);
-
-//     return (
-//         <Link  to={`/donationDetails/ ${id}`}>
-//             <div className="text-left " >
-//                 <div className="card card-compact bg-base-100 shadow-xl " style={{ backgroundColor: cardBgColor }}>
-//                     <figure>
-//                         <img className="w-full h-[250px]" src={img} alt="" />
-//                     </figure>
-//                     <div className="card-body mt-4">
-//                         <h3 className="text-base pl-3 py-[2px] rounded-lg mr-[270px] flex" style={{ backgroundColor: categoryBgColor }}>{category}</h3>
-//                         <h2 className="font-bold text-2xl mt-3" style={{ color: TEXTColor }}>{title}</h2>
-//                     </div>
-//                 </div>
-//             </div>
-//         </Link>
-//     );
-// };
-
-// export default SingleCategory;
-
-
 import { Link, useNavigate } from "react-router-dom";
 
-const SingleCategory = ({singleData}) => {
-    const {id, img, category, title, cardBgColor, categoryBgColor, TEXTColor } = singleData;
-    // console.log(singleData);
-
-    const navigate = useNavigate()
-
-    const handleDonationDetails = ()=>{
-       navigate(`/donationDetails/ ${id}`)
-    }
+const SingleCategory = ({ singleData }) => {
+    const { id, img, category, title, cardBgColor, categoryBgColor, TEXTColor } = singleData;
 
     return (
-        <div  onClick={()=>handleDonationDetails({id})}>
+        <Link to={`/donationDetails/${id}`}>
             <div className="text-left " >
                 <div className="card card-compact bg-base-100 shadow-xl " style={{ backgroundColor: cardBgColor }}>
                     <figure>
                         <img className="w-full h-[250px]" src={img} alt="" />
                     </figure>
                     <div className="card-body mt-4">
-                        <h3 className="text-base px-2 py-1 rounded-lg  flex max-w-fit font-medium" style={{ backgroundColor: categoryBgColor, color:TEXTColor }}>{category}</h3>
+                        <h3 className="text-base px-2 py-1 rounded-lg  flex max-w-fit font-medium" style={{ backgroundColor: categoryBgColor, color: TEXTColor }}>{category}</h3>
                         <h2 className="font-bold text-2xl mt-3" style={{ color: TEXTColor }}>{title}</h2>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
